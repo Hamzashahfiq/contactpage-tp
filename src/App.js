1c0b83e8c859fa37d77917a34e4995bf48ed3835
Muhammad Hamza <hamzashafique535@gmail.com>
2022-09-11 00:45:20 +0500

    complete contact page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import ContactPage from './modules/contactPage/ContactPage';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
     <ContactPage />
+    <ToastContainer />
     </>
   );
 }
